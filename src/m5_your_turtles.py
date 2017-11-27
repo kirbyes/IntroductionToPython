4074f6c590ab6a62cd83669686fea5965a1dcc92
kirbyes <kirbyes@rose-hulman.edu>
2017-11-27 13:01:53 -0500

    I completed the TODO's

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Eric Kirby.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,36 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+window = rg.TurtleWindow()
+turtle1 = rg.SimpleTurtle('turtle')
+turtle1.pen = rg.Pen('red', 2)
+turtle1.speed = 10
+
+size = 50
+
+for k in range(8):
+    turtle1.draw_regular_polygon(k+3, size)
+
+turtle2 = rg.SimpleTurtle("turtle")
+turtle2.pen = rg.Pen('green', 2)
+turtle2.speed = 10
+
+turtle2.pen_up()
+turtle2.right(90)
+turtle2.forward(150)
+
+for k in range(8):
+    turtle2.pen_down()
+    turtle2.draw_circle(size)
+
+    size = size + 10
+
+    turtle2.pen_up()
+    turtle2.right(90)
+    turtle2.forward(10)
+    turtle2.left(90)
+
+
+window.close_on_mouse_click()
